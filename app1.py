e76cd03a3ce1f89e4501ca989b7186181d9f2d72
jils <jilspatel141204@gmail.com>
2026-04-10 01:08:57 -0400

    input type will always give out string becasue thats how python works

--- app1.py
+++ app1.py
@@ -1,1 +1,12 @@
-print("Enter todo list:")
+user_rompt = ("To Do List:")
+todo1 = input(user_rompt)
+todo2 = input(user_rompt)
+todo3 = input(user_rompt)
+todo4 = input(user_rompt)
+
+All_todo = [todo1,todo2,todo3 ,todo4]
+print(All_todo)
+
+
+print(type(All_todo))
+print(type(todo1))
